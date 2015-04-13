376052ad28bb37ae9c1cb64bc26a956ec0c016a0
wangbaoguo <1402814003@qq.com>
2015-04-13 23:11:59 +0800

    add load-grunt-tasks,time-grunt; answer Q1 and Q2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
-//Q1:cwd什么意思
-//Q2:怎么写后缀名
+//Q1:cwd什么意思    A:相对于src的询问路径(一言以蔽之：在cwd下找src的内容)
+//Q2:怎么写后缀名   A:ext属性指定后缀名，extDot属性(first,last)指定从哪个'.'开始修改后缀
 
 module.exports = function(grunt){
+
+    require('time-grunt')(grunt);
+
+    /* load npm tasks */
+    require('load-grunt-tasks')(grunt);
+
+
 	/* last modified time 暂时没用*/
 	function lastModified(minutes) {
 		return function(filepath) {
@@ -70,6 +77,17 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	// ...
 
+    /* 
+        组合的task 
+        method1 : array
+        method2 : function
+    */
 	grunt.registerTask('default', ['clean', 'concurrent:develop']);
+
+    grunt.registerTask('default',function (target) {
+        //target: task name        
+        grunt.task.run(['clean','concurrent:develop']);
+
+    });
 	//...
 }
